59baa2331e48aa8382a5b3375ac6b11b0aff15da
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-11 19:45:54 +0300

    creating bottom navigation on the home page

--- app/src/main/java/com/example/saveprojectidea/HomePage.java
+++ app/src/main/java/com/example/saveprojectidea/HomePage.java
@@ -1,14 +1,24 @@
 package com.example.saveprojectidea;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class HomePage extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class HomePage extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+         bottomNavigationView = findViewById(R.id.bottem_NavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
