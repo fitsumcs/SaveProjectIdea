af3989e870a46b6089abddf9afbc5cfd3d812d32
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-11 20:10:23 +0300

    Adding sign out functionality

--- app/src/main/java/com/example/saveprojectidea/User_Setting.java
+++ app/src/main/java/com/example/saveprojectidea/User_Setting.java
@@ -1,13 +1,18 @@
 package com.example.saveprojectidea;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class User_Setting extends Fragment {
@@ -41,5 +46,24 @@ public class User_Setting extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = (Button)view.findViewById(R.id.button_logout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(),SignIn.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
