669a187c7b78504e70bd23143ed9f7d5a1766da9
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-11 18:53:47 +0300

    Adding Loged User activity and checking loged user onstart

--- app/src/main/java/com/example/saveprojectidea/MainActivity.java
+++ app/src/main/java/com/example/saveprojectidea/MainActivity.java
@@ -6,21 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
         //splash screen
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent intent = new Intent(MainActivity.this,SignIn.class);
-                startActivity(intent);
-                finish();
+                if (user != null) {
+                    Intent intent = new Intent(MainActivity.this,HomePage.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Intent intent = new Intent(MainActivity.this,SignIn.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+
 
             }
         },3000);
