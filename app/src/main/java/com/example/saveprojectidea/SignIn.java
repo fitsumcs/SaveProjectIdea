bb3c429db67caf1de64f6997c7b409de2ab9e69e
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-11 20:49:03 +0300

    Adding password visiblity

--- app/src/main/java/com/example/saveprojectidea/SignIn.java
+++ app/src/main/java/com/example/saveprojectidea/SignIn.java
@@ -1,26 +1,27 @@
 package com.example.saveprojectidea;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
 
-    EditText email,password;
+    EditText email;
+    TextInputEditText password;
     TextView editText_signup;
     ProgressDialog progressDialog;
 
@@ -31,7 +32,7 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         email = (EditText) findViewById(R.id.editText_Email);
-        password = (EditText)findViewById(R.id.editText_Password);
+        password = (TextInputEditText)findViewById(R.id.editText_Password);
         progressDialog = new ProgressDialog(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -77,7 +78,8 @@ public class SignIn extends AppCompatActivity {
 
                 if(task.isSuccessful())
                 {
-                    Toast.makeText(SignIn.this,"Successfully Loged!!",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignIn.this,HomePage.class);
+                    startActivity(intent);
 
                 }
                 else{
