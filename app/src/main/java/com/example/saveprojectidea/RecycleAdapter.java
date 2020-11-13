19955ddc7bc9eb89f34d64800757f193e829b52f
Fitsum Alemu <fitsumcs@gmail.com>
2020-11-13 13:09:13 +0300

    Adapter for the recycler view ready

---
+++ app/src/main/java/com/example/saveprojectidea/RecycleAdapter.java
@@ -0,0 +1,77 @@
+package com.example.saveprojectidea;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecycleAdapter  extends RecyclerView.Adapter<RecycleAdapter.ProjectIdeaHandler> {
+
+    Context context;
+    ArrayList<ProjectIdeas> projectIdeas;
+
+    public RecycleAdapter(Context context, ArrayList<ProjectIdeas> projectIdeas)
+    {
+
+        this.context = context;
+        this.projectIdeas = projectIdeas;
+
+
+    }
+
+    @NonNull
+    @Override
+    public ProjectIdeaHandler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row,parent,false);
+        ProjectIdeaHandler projectIdeaHandler = new ProjectIdeaHandler(view);
+        return projectIdeaHandler;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProjectIdeaHandler holder, int position) {
+
+        ProjectIdeas ideaOfProject = projectIdeas.get(position);
+        String title = ideaOfProject.getProjectTitle();
+        String description = ideaOfProject.getProjectDescription();
+        String regestiredDate = ideaOfProject.getProjectDate();
+
+        //set the view
+        holder.tv_title.setText(title);
+        holder.tv_description.setText(description);
+        holder.tv_date.setText(regestiredDate);
+    }
+
+    @Override
+    public int getItemCount() {
+        return projectIdeas.size();
+    }
+
+    public class ProjectIdeaHandler extends RecyclerView.ViewHolder{
+
+        TextView tv_title, tv_description, tv_date;
+        ImageView im_edit,im_delete;
+
+        public ProjectIdeaHandler(@NonNull View itemView) {
+            super(itemView);
+
+            tv_title = (TextView) itemView.findViewById(R.id.textView_Title);
+            tv_description = (TextView) itemView.findViewById(R.id.textView_Description);
+            tv_date = (TextView) itemView.findViewById(R.id.textView_Date);
+
+            im_edit = (ImageView) itemView.findViewById(R.id.image_edit);
+            im_delete = (ImageView) itemView.findViewById(R.id.image_delete);
+
+
+        }
+    }
+
+
+}
